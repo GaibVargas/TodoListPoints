2aeb8ac06207127c1a08a36ff5e41d26fc26a5c2
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2020-06-16 19:49:20 -0300

    Create Home page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import Home from './src';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <Home />
   );
 }
